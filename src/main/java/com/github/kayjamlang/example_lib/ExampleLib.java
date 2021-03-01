f1bc4f6832b5083ee08c4cd4a9faece64815bb54
Domaman202 <vip.domaman@mail.ru>
2021-03-01 18:58:16 +0300

    .

---
+++ src/main/java/com/github/kayjamlang/example_lib/ExampleLib.java
@@ -0,0 +1,37 @@
+package com.github.kayjamlang.example_lib;
+
+import com.github.kayjamlang.core.Type;
+import com.github.kayjamlang.core.containers.Function;
+import com.github.kayjamlang.executor.libs.Library;
+
+public class ExampleLib extends Library {
+    public ExampleLib() throws Exception {
+        // Main constructor
+        System.out.println("ExampleLib loading!");
+
+        // Simple global function "foo"
+        functions.add(new LibFunction("foo", ((mainContext, context) -> {
+            System.out.println(context.variables.get("str"));
+            return "Test String";
+        }), new Function.Argument(Type.STRING, "str")));
+
+        // Simple class "clss"
+        classes.put("clss", new LibClass("clss", clazz -> {
+            // Simple companion
+            clazz.setCompanion(new LibObject(object -> {
+                // Simple static function "Hi"
+                object.addFunction(new LibFunction("Hi", (mainContext, context) -> {
+                    System.out.println("Hi!");
+                    return null;
+                }));
+            }));
+            // Simple no static variable "i"
+            clazz.addVariable("i", 0);
+            // Simple no static function "foo"
+            clazz.addFunction(new LibFunction("foo", (mainContext, context) -> {
+                System.out.println("i = " + context.parentContext.variables.get("i"));
+                return null;
+            }));
+        }));
+    }
+}
---
+++ src/test/java/com/github/kayjamlang/example_lib/Main.java
@@ -0,0 +1,29 @@
+package com.github.kayjamlang.example_lib;
+
+import com.github.kayjamlang.executor.Executor;
+
+public class Main {
+    // Simple lib test
+    public static void main(String[] args) throws Exception {
+        Executor executor = new Executor();
+
+        // Load ExampleLib
+        System.out.println("\n\nLib loading!");
+        executor.addLibrary(new ExampleLib());
+
+        // Global function "foo" test
+        System.out.println("\n\nCall function \"foo\"!");
+        System.out.println(executor.execute("{return foo(\"Hello string!\")}"));
+
+        // Class "clss" test
+        System.out.println("\n\nTest class \"class\"!");
+        // Companion test
+        executor.execute("{clss::Hi()}");
+        // Normal class test
+        executor.execute(
+                "{var obj = clss();" +
+                        "obj.i = 12;" +
+                        "obj.foo()}"
+        );
+    }
+}
